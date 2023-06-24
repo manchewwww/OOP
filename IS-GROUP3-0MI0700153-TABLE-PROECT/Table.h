ea34ee384471fd2e9ff4b6fe6cb30aae11cecdea
manchewwww <116086924+manchewwww@users.noreply.github.com>
2023-06-24 14:19:35 +0300

    PROJECT

---
+++ IS-GROUP3-0MI0700153-TABLE-PROECT/Table.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include "Cell.h"
+
+class Table {
+public:
+	Table() = default;
+	~Table();
+	unsigned getRows() const;
+	unsigned getColumns() const;
+	bool isOpenTable() const;
+	void setOpen();
+	std::vector<std::vector<Cell*>>& getTable();
+	const Cell* getCell(const unsigned row, const unsigned column) const;
+private:
+
+private:
+	bool isOpen = false;
+	std::vector<std::vector<Cell*>> table;
+};
